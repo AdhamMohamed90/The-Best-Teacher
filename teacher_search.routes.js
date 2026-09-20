0d173b7f6aa87d5fd884ea2bd9519ab372d90db8
Adham <adhammohamedhassan2582007@gmail.com>
2026-09-20 12:44:39 +0300

    Delete teacher_search.routes.js

--- teacher_search.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {search, getTeacher} = require("../controllers/teacher_search.controller");
-
-router.get("/teachers/search",search);
-router.get("/teachers/:id", getTeacher);
-
-module.exports = router;
